0dd009933301c410367ef1cfc4111773c3ec76ae
gabriel <80391539+GabrielPRomero@users.noreply.github.com>
2021-12-29 14:38:45 +0200

    added email mechanism

--- api/covidtech/covidtech/settings.py
+++ api/covidtech/covidtech/settings.py
@@ -128,6 +128,6 @@ EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "gabrielpitaromero@gmx.com"
 EMAIL_HOST = "smtp.gmx.com"
-EMAIL_HOST_PASSWORD = "dega0505"
+EMAIL_HOST_PASSWORD = "apppwd13"
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
